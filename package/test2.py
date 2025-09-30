a6538fe947f25806114dcc7f942d8da7acb1a5cb
ankur0704 <sonavaneankur@gmail.com>
2025-09-30 20:29:06 +0530

    importing modules and packages in Python

---
+++ package/test2.py
@@ -0,0 +1,9 @@
+from package.maths import *
+from package.subpackages.mult import multiply
+
+print(addition(2,3))
+print(substraction(4,3))    
+print(multiply(4,5))
+
+
+## python -m package.test2.py  (command to run in terminal)
